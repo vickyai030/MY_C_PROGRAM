43ae90f11db1b273eb9aed01a3913909fe8f5ce4
vickyai030 <vikasgouda5@gmail.com>
2025-11-17 19:13:16 +0530

    Create name age loc.c

---
+++ name age loc.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int name,age,location;
+    printf("enter a name\n");
+    scanf("%d",&name);
+    printf("enter a age\n");
+    scanf("%d",&age);
+    printf("enter a location\n");
+    scanf("%d",&location);
+    
+    
+}
