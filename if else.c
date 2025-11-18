228d7f2b55983cbdfd19ae323593ef5c3bad96a7
vickyai030 <vikasgouda5@gmail.com>
2025-11-18 10:57:48 +0530

    Create if else.c

---
+++ if else.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a=10;
+    if(a>0){
+       printf("a is positive number\n");
+    }
+    else{
+        printf("a is negitive\n"); 
+    }
+      
+    
+    
+}
+     
