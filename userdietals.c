d55b8fffa4c2c57a947643584f6ad582bed87ae1
vickyai030 <vikasgouda5@gmail.com>
2025-11-18 15:38:38 +0530

    Create userdietals.c

---
+++ userdietals.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int vikas()
+
+{
+    char name,department;
+    int age;
+    printf("enter a name\n");
+    scanf("%c",&name);
+    printf("enter a age\n");
+    scanf("%d",&age);
+    printf("enter a department\n");
+    scanf("%c",department);
+   
+}
+int main() {      
+    vikas();
+    
+    
+    
+}
