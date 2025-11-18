6e1fe72f82ae7b794798c23683f135234012a5c0
vickyai030 <vikasgouda5@gmail.com>
2025-11-18 11:03:56 +0530

    Create else if .c

---
+++ else if .c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+    int marks=60;
+    if(marks>90){
+        printf("merit");
+    }
+    else if(marks>=80){
+        printf("distinction");
+    }
+    else if(marks>=70){
+        printf("good");
+    }
+    else if(marks>=60){
+        printf("average");
+    }
+    else if (marks>=50){
+        printf("just pass");
+    }
+    else{
+        printf("fail");
+    }
+   
+    
+    
+}
+     
