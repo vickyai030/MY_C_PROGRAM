c83b75ddd2f9ed5bf42a8ef4c8358d70b610d4e6
vickyai030 <vikasgouda5@gmail.com>
2025-11-19 09:24:05 +0530

    Update vowel or consonant.c

--- vowel or consonant.c
+++ vowel or consonant.c
@@ -4,14 +4,17 @@ int main() {
  char ch;
     printf("enter a char\n");
     scanf("%s",&ch);
-    if ((ch>='a'&&ch>='z')||(ch<='A'&&ch>='Z')){
-        if((ch=='a'||ch=='e' ch=='i'||ch=='o'||ch=='u')(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')){
+   {
+        if((ch=='a'||ch=='e' ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')){
             printf("ch is vowel\n",ch);
         }
-        else{
+        else if ((ch>='a'&&ch>='z')||(ch<='A'&&ch>='Z')){
+        
             printf("ch is consonant\n",ch);
-        }
-    }
+       
+    }else{
+    printf("invalid ch\n");
+   }
     
 }
     
