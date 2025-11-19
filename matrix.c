41469e9dff18e6ce938f16399acc7a4a1767882b
vickyai030 <vikasgouda5@gmail.com>
2025-11-19 12:20:26 +0530

    Create matrix.c

---
+++ matrix.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main() {
+int rows, cols;
+printf("Enter the number of rows and columns: ");
+scanf("%d %d", &rows, &cols);
+int a[rows][cols], b[rows][cols], sum[rows][cols];
+printf("Enter elements of matrix A:\n");
+for (int i = 0; i < rows; i++) {
+for (int j = 0; j < cols; j++) {
+scanf("%d", &a[i][j]);
+}
+}
+printf("Enter elements of matrix B:\n");
+for (int i = 0; i < rows; i++) {
+
+
+for (int j = 0; j < cols; j++) {
+scanf("%d", &b[i][j]);
+}
+}
+for (int i = 0; i < rows; i++) {
+for (int j = 0; j < cols; j++) {
+sum[i][j] = a[i][j] + b[i][j];
+}
+}
+printf("Resultant Matrix:\n");
+for (int i = 0; i < rows; i++) {
+for (int j = 0; j < cols; j++) {
+printf("%d ", sum[i][j]);
+}
+printf("\n");
+}
+return 0;
+}
