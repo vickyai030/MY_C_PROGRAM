6dd4e9c9d23b0171dcdd42fbf488879aebd12969
vickyai030 <vikasgouda5@gmail.com>
2025-11-20 10:52:59 +0530

    Create string compare.c

---
+++ string compare.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char name[10];
+    printf("enter a name");
+    scanf("%s",&name);
+     int c=strcmp(name,"vikas");
+    printf("%d",c);
+    
+    
+}
