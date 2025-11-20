42ac907316d02c415bcda8a1a4ec241101b10048
vickyai030 <vikasgouda5@gmail.com>
2025-11-20 10:27:14 +0530

    Create palidrome.c

---
+++ palidrome.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int num=121,org,rev=0,rem;
+    org=num;
+    while(num!=0){
+        rem=num%10;
+        rev=rev*10+rem;
+        num=num/10;
+        
+    }
+    if(org==rev){
+        printf("num is pal");
+    }
+    else{
+        printf("not pal");
+    }
+    
+}
