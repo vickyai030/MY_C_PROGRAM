f1c6c82669a28975cea50671680832713ce411d4
vickyai030 <vikasgouda5@gmail.com>
2025-11-20 10:47:00 +0530

    Create string concadnation.c

---
+++ string concadnation.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char name[10];
+    printf("enter a name");
+    scanf("%s",&name);
+    strcat(name,"patil");
+    printf("%s",name);
+    
+    
+}
