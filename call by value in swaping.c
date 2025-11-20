9aa81f1fdd680a20a8130fbe1bf127833289bd11
vickyai030 <vikasgouda5@gmail.com>
2025-11-20 14:03:18 +0530

    Create call by value in swaping.c

---
+++ call by value in swaping.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int swap(int a,int b);{
+    int temp;
+    temp=a;
+    a=b;
+    b=temp;
+    printf("%d%d",a,b)
+}
+int main(){
+    int (x ,y);
+    printf("%d%d"x,y);
+    swap(10,40);
+    printf("%d%d",x,y)
+}
