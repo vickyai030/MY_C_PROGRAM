cf1a0d5f1145d102e238dd3086e80d7209388f2e
vickyai030 <vikasgouda5@gmail.com>
2025-11-22 12:45:43 +0530

    Create  contact name first and last.c

---
+++ contact name first and last.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+    char first[50]="vikas",last[50]="patil",full[200];
+    int i=0,j=0,length=0,maxlength=20;
+    while(first[i] !='\0'){
+        full[length]=first[i];
+        i++;
+        length++;
+    }
+    full[length]='\0';
+    length++;
+    while(last[i] !='\0'){
+    full[length]=last[i];
+    i++;
+    length++;
+    }
+    full[length]='\0';
+    if(length<=maxlength){
+        printf("name =%s\n",full);
+        printf("length =%d\n",length);
+        printf("name is fitted");
+    }
+    else{
+        printf("name =%s",full);
+        printf("length %d",length);
+        printf("name is not fitted ");
+    }
+    
+}
